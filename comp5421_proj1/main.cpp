75ea369d8e1a73d5487a38fa9bfc634f067273b9
Wa WongTak <wongtakwa@WongTakdeMacBook-Pro.local>
2017-05-04 01:56:00 +0800

    Initial Commit

---
+++ comp5421_proj1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  comp5421_proj1
+//
+//  Created by WongTak Wa on 4/5/2017.
+//  Copyright © 2017年 wongtakwa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
